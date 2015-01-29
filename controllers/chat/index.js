5bfd3c0746dddaf60b6075830cf93602caa9797f
javier <javier.valdez@outlook.es>
2015-01-29 11:05:32 -0700

    session

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -4,13 +4,23 @@ app.set('views', __dirname + '/views');
 
 
 app.get('/', function(request, response) {
-response.render('index', {
-title: 'login'
+	response.render('index', {
+		title: 'login'
+	});
 });
+
+app.get('/chat', function(request, response) {
+	console.log(request.body);
+	response.render('chat', {
+		user: 'login'
+	});
 });
 
 app.post('/login', function(req, res) {
-	console.log(req.body)
-
+	console.log(req.body);
+	res.redirect('/chat');
+	res.render('chat', {
+		user: 'login'
+	});
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,20 @@
 function login () {
-	console.log("sup")
+
 	var user=document.getElementById('user').value;
 	var pass=document.getElementById('pass').value;
-	
-	var encrypted = CryptoJS.TripleDES.encrypt("Message", "Secret Passphrase");
+	var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+    var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
 	console.log(encrypted);
-
-    var decrypted = CryptoJS.TripleDES.decrypt(encrypted, "Secret Passphrase");
-	console.log(decrypted);
-
+	console.log(CryptoJS.enc.Latin1.stringify(decrypted));
+	var data="user="+user+"&pass="+pass;
+	$.ajax({
+		data:  data,
+        url:   '/login',
+        type:  'post',
+        async:false,
+       success:  function (response) {
+       	console.log(response)
+       }
+	})
 }
 
-/*function encrypt (data) {
-	var salt = CryptoJS.lib.WordArray.random(128/8);
-    /*var key128Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 128/32 });
-    var key256Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 256/32 });
-    var key512Bits = CryptoJS.PBKDF2("Secret Passphrase", salt, { keySize: 512/32 });
-    
-    var key512Bits1000Iterations = CryptoJS.PBKDF2(data, salt, { keySize: 512/32, iterations: 100 });
-    console.log(decryptString(key512Bits1000Iterations.toString(),512/32,salt.toString()));
-	return salt+key512Bits1000Iterations;
-}
-*/
-
-
- 
- 
